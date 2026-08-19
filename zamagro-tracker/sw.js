2dc2e2c06f32eb4e2363303930b954bae6347f6d
MyDepotMaster <chongos909@gmail.com>
2026-08-20 01:02:15 +0200

    Add files via upload

--- zamagro-tracker/sw.js
+++ zamagro-tracker/sw.js
@@ -5,22 +5,20 @@
 
 // Bump this on every deploy so returning users pick up the new index.html
 // instead of being stuck on a stale cached copy.
-const CACHE_VERSION = 'zamagro-shell-v3';
+const CACHE_VERSION = 'zamagro-shell-v4';
 
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './icon-72x72.png',
-  './icon-96x96.png',
-  './icon-128x128.png',
-  './icon-144x144.png',
-  './icon-152x152.png',
-  './icon-192x192.png',
-  './icon-384x384.png',
-  './icon-512x512.png',
-  './apple-touch-icon.png',
-  './favicon.webp'
+  './Icons/icon-72x72.png',
+  './Icons/icon-96x96.png',
+  './Icons/icon-128x128.png',
+  './Icons/icon-144x144.png',
+  './Icons/icon-152x152.png',
+  './Icons/icon-192x192.png',
+  './Icons/icon-384x384.png',
+  './Icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
