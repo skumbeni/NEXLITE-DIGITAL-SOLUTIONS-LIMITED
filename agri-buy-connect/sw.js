5c388b5471f38f44e58e001585819b78f4d3bea8
MyDepotMaster <chongos909@gmail.com>
2026-09-07 08:19:38 +0200

    Add files via upload

--- agri-buy-connect/sw.js
+++ agri-buy-connect/sw.js
@@ -10,7 +10,7 @@
  * Bump CACHE_NAME on every deploy so returning users pick up the new
  * shell instead of a stale cached copy.
  */
-const CACHE_NAME = 'agribuy-shell-v2';
+const CACHE_NAME = 'agribuy-shell-v3';
 const APP_SHELL = [
   './',
   './index.html',
